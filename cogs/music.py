e454f76a84e93bc3b50c4143a414fa796f9e9abb
Nash Sauter <nashsauter1@gmail.com>
2023-02-14 21:37:06 -0700

    play queue and end listener
    skip command
    
    switch respond and queue order

--- cogs/music.py
+++ cogs/music.py
@@ -2,6 +2,11 @@ import discord
 from discord.ext import commands, bridge
 import wavelink
 
+class CustomPlayer(wavelink.Player):
+    def __init__(self):
+        super().__init__()
+        self.queue = wavelink.Queue()
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -26,28 +31,50 @@ class Music(commands.Cog):
     async def on_wavelink_node_ready(self, node):
         print(f"Node {node.identifier} is ready")
 
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, player, track, reason):
+        if not player.queue.is_empty:
+            next_track = player.queue.get()
+            await player.play(next_track)
 
     @bridge.bridge_command(name="play")
     async def play(self, ctx, search: str):
-      vc = ctx.voice_client # define our voice client
+        vc = ctx.voice_client # define our voice client
 
-      if not vc: # check if the bot is not in a voice channel
-        vc = await ctx.author.voice.channel.connect(cls=wavelink.Player) # connect to the voice channel
-      if ctx.author.voice.channel.id != vc.channel.id: # check if the bot is not in the voice channel
-        return await ctx.respond("You must be in the same voice channel as the bot.") # return an error message
+        if not vc: # check if the bot is not in a voice channel
+            vc = await ctx.author.voice.channel.connect(cls=wavelink.Player) # connect to the voice channel
+        if ctx.author.voice.channel.id != vc.channel.id: # check if the bot is not in the voice channel
+            return await ctx.respond("You must be in the same voice channel as the bot.") # return an error message
 
-      song = await wavelink.YouTubeTrack.search(query=search, return_first=True) # search for the song
-      if not song: # check if the song is not found
-        return await ctx.respond("No song found.") # return an error message
+        song = await wavelink.YouTubeTrack.search(query=search, return_first=True) # search for the song
+        if not song: # check if the song is not found
+            return await ctx.respond("No song found.") # return an error message
 
-      await vc.play(song) # play the song
-      await ctx.respond(f"Now playing: `{vc.source.title}`") # return a message
+        if vc.is_playing():
+            await ctx.respond(f"Queued: `{song.title}`") # return a message
+            await vc.queue.put(item=song)
+        else:
+            await vc.play(song) # play the song
+            await ctx.respond(f"Now playing: `{vc.source.title}`") # return a message
 
     @bridge.bridge_command(name="stop")
     async def stop(self, ctx):
         vc = ctx.voice_client
         await vc.disconnect()
         await ctx.respond("Player has stopped")
+    
+    @bridge.bridge_command(name="skip")
+    async def skip(self, ctx):
+        vc = ctx.voice_client
+        if vc:
+            if not vc.is_playing():
+                return await ctx.send("Nothing is playing.")
+            if vc.queue.is_empty:
+                return await vc.stop()
+
+            await vc.seek(vc.track.length * 1000)
+            if vc.is_paused():
+                await vc.resume()
 
     @commands.Cog.listener()
     async def on_ready(self):
