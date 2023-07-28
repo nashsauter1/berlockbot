e11cd2205d63b9cc599b57f14c8f54066338cd56
Nash Sauter <nashsauter1@gmail.com>
2023-07-27 18:01:51 -0600

    riichi

--- cogs/memes.py
+++ cogs/memes.py
@@ -63,5 +63,12 @@ class Memes(commands.Cog):
         await ctx.message.add_reaction(emoji)
         await ctx.respond(':yum:yummy!:yum:')
 
+    @commands.command()
+    async def riichi(self, ctx):
+        emoji = discord.utils.get(ctx.guild.emojis, name='ohmydog')
+        await ctx.message.add_reaction(emoji)
+        await ctx.respond("Look at this absolute fucking legend sitting in front of me. This motherfucker, this smug mother fucker right here knows exact;y what I/I'm going for. He knows I'm in fucking tenpai for a dealer baiman, and you know what he does? He riichi. He doesn't even riichi a good wait mind you, no no no, mister can't afford throwing his precious red five to improve his wait a bit. No, he slams down his balls the size of grapefruits on the table and riichi this fucking KANCHAN. And you know what happens ? Of course you do. 'TSUMO, aaah yatto kita'. Ippatsu. He doesn't even look at me. He just proceeds with the game, because for him this is just natural. While I wonder where everything went wrong. ' What was I supposed to do?'. Look at my pond, look at it. I'm overdosing harder then a drug addict on luck, yet I'm not the one who wins/ I'm the one that gets to look like an utter buffoon. A pawn following what mahjong tells him to go for, as a joke. He even fed me all of them. A calculated risk, for he knew, he new that I'd fall for it. Calling them all one by one like a maniac, merely letting myself be taken by the flow, while he is harnessing it. The game was over before it even began. While he kept playing like usual, I was possessed by paranoia, doubting everything, lost in the dark, not knowing what I should even believe in, if there's even any hope for players like me. That's when I realized, mahjong isn't a game. It's a play. A cruel Theatre, as we're led to believe everyone hold the same role. But it's wrong. There are those the tiles will be kind to. They'll give them hard times once in a while, but they are undoubtedly love by them. To make up for that love they need to torment the other bunch. That's where I fit the bill. I'm comic relief, the guy at the table that everyone gets a good laugh out of. In a sense I'm needed. But I didn't ask for this. i just wanted to play mahjong, wanted to believe I, too, could, maybe, be loved.")
+
+
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Memes(bot)) # add the cog to the bot
