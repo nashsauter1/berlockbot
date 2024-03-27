0654b72fab99f1720f2b71b4958ff8d6b73efe35
Nash Sauter <nashsauter1@gmail.com>
2024-03-26 22:47:59 -0600

    remove minimal and remove test

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -7,13 +7,6 @@ class GPT(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @bridge.bridge_command(brief = "cat")
-    async def cat(self, ctx, text):
-        cat_text = berlock_gpt.newcatGPT(text)
-        emoji = discord.utils.get(ctx.guild.emojis, name='ohmydog')
-        await ctx.respond(cat_text)
-        await ctx.message.add_reaction(emoji)
-
     @bridge.bridge_command(brief = "CATify")
     async def catify(self, ctx, text):
         cat_text = berlock_gpt.newcatGPT(text)
@@ -23,7 +16,7 @@ class GPT(commands.Cog):
         embed.set_author(name="Berlock Bot",
                          icon_url="https://images-ext-2.discordapp.net/external/XaZD0LjdRFtLqJKRHD96hbM3Yrxo0Nr2bQrkz2yN7Uk/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/1027808278551461988/d9ce0c74f3aa9fb61e44fb9b186dd9bd.png")
         embed.add_field(name="Catify =^._.^=",
-                        value=f"```test```")
+                        value=f"```{cat_text}```")
         emoji = discord.utils.get(ctx.guild.emojis, name='ohmydog')
         await ctx.respond(embed = embed)
         await ctx.message.add_reaction(emoji)
