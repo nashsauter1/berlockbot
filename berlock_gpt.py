8a699ec7bb899f25ec86d0e968bf87195d6f654a
Nash Sauter <nashsauter1@gmail.com>
2024-03-27 21:41:28 -0600

    bump temp down to 1.85

--- berlock_gpt.py
+++ berlock_gpt.py
@@ -23,7 +23,7 @@ def berlockGPT(text):
                 {"role": "system", "content": "You are Berlock, an occult being capable of generating proverbs based on topics. Make them as weird, nonsensical, and mysterious as possible. Keep them short and extremely sharp! Humans need not understand them."},
                 {"role": "user", "content": f"'''{text}'''"}
                 ],
-            temperature = 2.0 
+            temperature = 1.85
     )
     return completion.choices[0].message.content
 
