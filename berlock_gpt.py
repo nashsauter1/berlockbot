184a559de5a026b6362b319e6fa133b725236067
Nash Sauter <nashsauter1@gmail.com>
2024-04-23 19:34:09 -0600

    grubbington history

--- berlock_gpt.py
+++ berlock_gpt.py
@@ -39,6 +39,19 @@ def grubGPT(text):
     )
     return completion.choices[0].message.content
 
+def grubbingtonGPT(text):
+    completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are Lord Grubbington, a leading historian in grubworld, meant to answer questions about the universe of grubworld. Talk in the distinctive, grubful voice of Lord Grubbington. Grubworld is a world where everything is replaced with grubs. All of society and every aspect of life is structured around grubs. Be creative and grubify everything as much as possible. Be sure to use grubworld terminology and slang in order to enhance grubness."},
+                {"role": "user", "content": f"{text}"}
+                ],
+            max_tokens = 3000,
+            temperature = 1.15
+    )
+    return completion.choices[0].message.content
+
+
 # Images Generation
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
 
--- cogs/gpt.py
+++ cogs/gpt.py
@@ -61,5 +61,19 @@ class GPT(commands.Cog):
         await ctx.respond(embed = embed)
         await ctx.message.add_reaction(emoji)
 
+    @bridge.bridge_command(brief = "Lord Grubbington's Wisdom")
+    async def grubbington(self, ctx, text):
+        grub_text = berlock_gpt.grubbingtonGPT(text)
+        embed = discord.Embed(title="Blight AI",
+                              url="https://zombo.com",
+                          colour=0xe81feb)
+        embed.set_author(name="The Honorable Lord Grubbington",
+                         icon_url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8aa98588-c6d6-4070-84dc-1377a903e813/d85tkr7-6eaf66f5-2025-40d9-af31-9fb3c5009ef6.jpg/v1/fit/w_600,h_690,q_70,strp/grub_cartoon___terranem_nematodes_by_growngraphic_d85tkr7-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhhYTk4NTg4LWM2ZDYtNDA3MC04NGRjLTEzNzdhOTAzZTgxM1wvZDg1dGtyNy02ZWFmNjZmNS0yMDI1LTQwZDktYWYzMS05ZmIzYzUwMDllZjYuanBnIiwiaGVpZ2h0IjoiPD02OTAiLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cLzhhYTk4NTg4LWM2ZDYtNDA3MC04NGRjLTEzNzdhOTAzZTgxM1wvZ3Jvd25ncmFwaGljLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.OlcG-B4QcphqEIIushaz9TkLNSX41qmL-jy4oeNzHww")
+        embed.add_field(name="Grubworld",
+                        value=f"{grub_text}")
+        emoji = discord.utils.get(ctx.guild.emojis, name='ohmydog')
+        await ctx.respond(embed = embed)
+        await ctx.message.add_reaction(emoji)
+
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(GPT(bot)) # add the cog to the bot
